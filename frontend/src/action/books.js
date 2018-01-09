9c98f707b0f788cde66240c356a79ded09a6665a
Ashkaan Jaberi <ashkaan14@gmail.com>
2018-01-08 16:33:21 -0800

    async add works

--- frontend/src/action/books.js
+++ frontend/src/action/books.js
@@ -1,14 +1,13 @@
 import uuid from 'uuid/v4';
+import superagent from 'superagent';
+let API = `${__API_URL__}/api/books`;
 
-export const create = ({title, author}) => ({
-  type: 'BOOK_CREATE',
-  payload: {
-    title,
-    author,
-    read: false, 
-    id: uuid(),
-  },
-});
+export const create = payload => store => {
+  superagent.post(API)
+    .send(payload)
+    .then(res => store.dispatch(createBook(res.body)))
+    .catch(console.error)
+};
 
 export const destroy = (payload) => ({
   type: 'BOOK_DELETE',
@@ -19,3 +18,14 @@ export const read = (payload) => ({
   type: 'BOOK_READ',
   payload,
 })
+
+
+const createBook = ({title, author}) => ({
+  type: 'BOOK_CREATE',
+  payload: {
+    title,
+    author,
+    read: false, 
+    id: uuid(),
+  },
+});
